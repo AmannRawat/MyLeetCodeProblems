2d6790e917db1ba263b6a002b06fbc1afd3d92fd
AmannRawat <amanrawat1509@gmail.com>
2026-01-20 21:23:02 +0530

    Unbounded Knapsack Patterns

---
+++ Dynamic Programming/Unbounded Knapsack/Unbounded Knapsack.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int solve(int N,vector<int>& val, vector<int>& wt,int W){
+        int dp[N+1][W+1];
+        
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if (wt[i-1] <= j) {
+                    dp[i][j] = max(
+                        val[i-1] + dp[i][j - wt[i-1]], //Main Differnece here
+                        dp[i-1][j]
+                    );
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][W];
+    }
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        return solve(val.size(),val,wt,capacity);
+        
+    }
+};
