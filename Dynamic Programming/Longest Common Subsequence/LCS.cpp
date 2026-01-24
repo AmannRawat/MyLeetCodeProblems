37b9fe379e857d886681ae81cdc277bfef2ab276
AmannRawat <amanrawat1509@gmail.com>
2026-01-24 22:45:38 +0530

    Longest Common Subsequence

---
+++ Dynamic Programming/Longest Common Subsequence/LCS.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int n,int m,string text1, string text2){
+        if(n==0 || m==0){
+            return 0;
+        }
+
+        if(text1[n-1]==text2[m-1]){
+            return 1+ solve(n-1,m-1,text1,text2); // +1 when you found common
+        }else{
+            return max(solve(n-1,m,text1,text2) , solve(n,m-1,text1,text2));
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        return solve(text1.size(),text2.size(),text1,text2);
+    }
+};
