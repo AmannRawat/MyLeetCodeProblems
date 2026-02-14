72a6fcde5ab81f91c408fe3f96be79e888bd8690
AmannRawat <amanrawat1509@gmail.com>
2026-02-14 23:26:08 +0530

    Add memoization solution for matrix chain multiplication

--- Dynamic Programming/Longest Common Subsequence/matix chain multiplication.cpp
+++ Dynamic Programming/Longest Common Subsequence/matix chain multiplication.cpp
@@ -16,3 +16,26 @@ class Solution {
         return ans;
     }
 };
+
+//Memoization
+class Solution {
+  public:
+    int dp[1001][1001];
+    int solve(int i,int j,vector<int> &arr){
+        if(i>=j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int minAns=INT_MAX;
+        for(int k=i;k<j;k++){
+            int temp= solve(i,k,arr) + solve(k+1,j,arr) + arr[i-1] * arr[k] * arr[j];
+            minAns=min(minAns,temp);
+        }
+        dp[i][j]=minAns;
+        return minAns;
+    }
+    int matrixMultiplication(vector<int> &arr) {
+        int n=arr.size();
+        memset(dp,-1,sizeof(dp));
+        int ans=solve(1,n-1,arr);
+        return ans;
+    }
+};
